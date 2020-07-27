a2d8accec6fdd70a429299e35404abf7cfe9d5a5
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-07-27 17:08:03 +0700

    click me

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IUser} from "../iuser";
 
 @Component({
@@ -9,40 +9,68 @@ import {IUser} from "../iuser";
 export class UserListComponent implements OnInit {
 
   title_page = 'Users'
-  users: IUser[] = [
-    {
-      id: 1,
-      name: 'nam',
-      email: 'nam@gamil.com'
-    },
-    {
-      id: 2,
-      name: 'vinh',
-      email: 'vinh@gamil.com'
-    },
-    {
-      id: 3,
-      name: 'duc',
-      email: 'duc@gamil.com'
-    }
-  ]
+  users: IUser[] = []
 
-  userFilter =[];
-  constructor() { }
+  // userFilter = [];
 
-  ngOnInit(): void {
-    this.userFilter = this.users;
+  constructor() {
   }
 
-  search(event){
+  ngOnInit(): void {
+    this.users = this.getUserList();
+    // this.userFilter = this.users;
+  }
+
+  getUserList(): IUser[] {
+    let users = [
+      {
+        id: 1,
+        name: 'nam',
+        email: 'nam@gamil.com'
+      },
+      {
+        id: 2,
+        name: 'vinh',
+        email: 'vinh@gamil.com'
+      },
+      {
+        id: 3,
+        name: 'duc',
+        email: 'duc@gamil.com'
+      }
+    ]
+    return users;
+  }
+
+  search(event) {
     let keyword = event.target.value;
-    this.userFilter = (keyword) ? this.filerByKeyword(keyword): this.users;
+    this.users = (keyword) ? this.filerByKeyword(keyword) : this.getUserList();
   }
 
   filerByKeyword(keyword) {
     return this.users.filter(user => {
-        return user.name.indexOf(keyword) != -1;
+      return user.name.indexOf(keyword) != -1;    //nếu keyword trùng tên user thì sẽ trả về 1 list danh sách các user có tên chứa keyword
     })
   }
+
+
+  delete(id) {
+
+    // for (let i = 0; i < this.userFilter.length; i++) {
+    //   if (id == this.userFilter[i].id) {
+    //     this.userFilter.splice(i, 1);
+    //     // console.log(this.users[i].name +" deleted")
+    //     console.log(this.userFilter.length);
+    //   }
+    // }
+    let userDeleted = [];
+    this.users.map(user => {      //dùng map để duyệt mảng
+      if(user.id != id){
+        userDeleted.push(user);
+      }
+    })
+    this.users = userDeleted;   //cuối cùng thì sẽ có 1 mảng user trừ user có id khi delete.
+  }
+
 
 }
